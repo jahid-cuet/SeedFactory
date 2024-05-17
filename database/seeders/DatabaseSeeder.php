996f7c1316e412a8641c26449a71d6d07967c253
Md.Jahid Hasan <jahidhasan5803733@gmail.com>
2024-05-17 23:29:07 +0600

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Teacher;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,7 +18,8 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
-        StudentSeeder::class
+        StudentSeeder::class,
+        TeacherSeeder::class
         ]);
 
 
---
+++ database/seeders/TeacherSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Teacher;
+
+class TeacherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Teacher::create([
+            'name'=>'hasan',
+            'email'=>'hasan@gmail.com'
+
+        ]);
+    }
+}
